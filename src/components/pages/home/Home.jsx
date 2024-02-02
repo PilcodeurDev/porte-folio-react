793a9abb5e6817d7f1e5a898d346395fb7d2383b
simon despres <simondprs62790@hotmail.fr>
2024-02-02 17:05:26 +0100

    responsive with min-width 640px

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -10,22 +10,26 @@ import "./Home.css";
 import Button from "../../button/Button";
 
 export default function Home() {
-  const btnText = 'A propos de moi'
-  const btnIcon = <FaArrowRightLong />
-  const btnHref = '/about'
+  const btnText = "A propos de moi";
+  const btnIcon = <FaArrowRightLong />;
+  const btnHref = "/about";
 
   return (
-    <div className="container m-auto h-full flex items-center">
-      <div className="profil fixed w-[30%]"></div>
-      <div className="center-text">
-        <div className="text-presentation">
-          <h1>
-            <font>je m'appelle</font>
-            <font>simon despres .</font>
-            <font>concepteur de</font>
-            <font>sites web</font>
+    <div className="container m-auto h-full flex flex-col gap-20 items-center relative lg:flex-row">
+      <div
+        className="profil w-80 h-80 mt-[15%] border-[#183966] rounded-full border-4
+      lg:w-[30%] lg:h-[70%] lg:fixed lg:mt-0 lg:top-[15%] lg:left-10 lg:rounded-[30px] lg:border-none
+      xl:top-[4%] xl:left-14 xl:h-[92%]"
+      ></div>
+      <div className=" w-2/3 lg:ml-[40%]">
+        <div className="relative -top-5 pr-5">
+          <h1 className="pl-12 dash">
+            <font className=" flex text-[#BBA06B]">je m'appelle</font>
+            <font className=" flex text-[#BBA06B]">simon despres .</font>
+            <font className=" flex ">concepteur de</font>
+            <font className=" flex ">sites web</font>
           </h1>
-          <p>
+          <p className="lg:w-[80%] text-xl my-5">
             Je suis concepteur Web ainsi que développeur front-end basé à la
             frontière Franco-Suisse. Je suis passionné par la création
             d'interface utilisateur interactive et dynamique qui captivera vos
