f5c0826ac7b953fa51053fb77af3aed14c5337b9
simon despres <simondprs62790@hotmail.fr>
2024-02-02 10:39:14 +0100

    clear comment in navbar.jsx

---
+++ src/components/pages/about/About.jsx
@@ -0,0 +1,12 @@
+/**
+ * The external imports
+ */
+// import { FaWhatsappSquare } from "react-icons/fa";
+/**
+ * The internal imports
+ */
+
+export default function About() {
+  return <div>About</div>;
+}
+
