a69dbc39bf2c8ca34f6e988f6ae1ca71534b8bb0
simon despres <simondprs62790@hotmail.fr>
2024-02-02 10:34:55 +0100

    navbar setup

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,69 @@
+/**
+ *  The external imports
+ */
+import { Link } from "react-router-dom";
+import { BsFillHouseFill } from "react-icons/bs";
+import { FaUser, FaBook, FaEnvelope, FaComments } from "react-icons/fa";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {
+//   faHouse,
+//   faUser,
+//   faBook,
+//   faEnvelope,
+//   faComment,
+// } from "@fortawesome/free-solid-svg-icons";
+
+/**
+ * The internal imports
+ */
+import "./Navbar.css";
+
+export default function Navbar() {
+  const menuIcons = [
+    {
+      id: 1,
+      path: "/",
+      text: "Accueil",
+      icon: <BsFillHouseFill className="icon" />,
+    },
+    {
+      id: 2,
+      path: "/about",
+      text: "A Propos",
+      icon: <FaUser className="icon" />,
+    },
+    {
+      id: 3,
+      path: "/portefolio",
+      text: "Portefolio",
+      icon: <FaBook className="icon" />,
+    },
+    {
+      id: 4,
+      path: "/contact",
+      text: "Contact",
+      icon: <FaEnvelope className="icon" />,
+    },
+    {
+      id: 5,
+      path: "/blog",
+      text: "Blog",
+      icon: <FaComments className="icon" />,
+    },
+  ];
+
+  return (
+    <nav className='navbar'>
+      <ul>
+        {menuIcons.map(({ id, path, icon, text }) => (
+          <li key={id} className='icon-box'>
+            <Link to={path}>
+              {icon}
+              <h6>{text}</h6>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
