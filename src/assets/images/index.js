e5e64a83fa205316f82ceae624bfca26447a02f6
simon despres <simondprs62790@hotmail.fr>
2024-02-02 13:22:02 +0100

    setup about page

---
+++ src/assets/images/index.js
@@ -0,0 +1,9 @@
+import css from './css.png';
+import figma from './figma.png';
+import html from './html.png';
+import js from './javascript.png';
+import pgSQL from './postgreSQL.png';
+import react from './react.png';
+import ruby from './ruby.png';
+
+export { css, figma, html, js, pgSQL, react, ruby };
