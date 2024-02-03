45b2612fbb611d0efa588a342227b16f55389fe6
simon despres <simondprs62790@hotmail.fr>
2024-02-03 11:54:05 +0100

    responsive title

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,5 +1,19 @@
+/**
+ * The external imports
+ */
+
+
+/**
+ * The internal imports
+ */
+import MyTitle from "../MyTitle";
+
 export default function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div className="w-[90%] m-auto">
+      <MyTitle span1={"prendre"} span2={"contact"} shadow={"contact"} />
+      <form action=""></form>
+      
+    </div>
+  );
 }
