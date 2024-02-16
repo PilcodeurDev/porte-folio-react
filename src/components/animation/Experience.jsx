cb6ff136c96bfc0427a8cf78d4cf3055bcf2a990
simon despres <simondprs62790@hotmail.fr>
2024-02-16 13:40:02 +0100

    add3D effect for text

--- src/components/animation/Experience.jsx
+++ src/components/animation/Experience.jsx
@@ -1,15 +1,31 @@
+/* eslint-disable react/no-unknown-property */
 /**
  * The external imports
  */
-import { useEffect, useState } from "react";
+import {
+  CameraControls,
+  Environment,
+  Float,
+  RenderTexture,
+  Text,
+  MeshReflectorMaterial,
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { EffectComposer, Bloom } from "@react-three/postprocessing";
+import { Suspense, useEffect, useState } from "react";
+import { Color } from "three";
 
 /**
  * The internal imports
  */
 import Profil3D from "../../components/animation/Profil3D";
+import GoodNeighbor from "../animation/GoodNeighbor";
+import { degToRad } from "three/src/math/MathUtils";
 
-export default function Experience() {
+const bloomColor = new Color("#fff");
+bloomColor.multiplyScalar(1.5);
+
+export default function Experience({ animatedElement }) {
   const [isBelowLG, setIsBelowLG] = useState(true);
 
   useEffect(() => {
@@ -26,44 +42,109 @@ export default function Experience() {
     };
   }, []);
 
-  return (
-    <>
-      {isBelowLG ? (
-        <div className="h-[320px]">
-          <Canvas
-            camera={{ position: [0, 0, 4.6], fov: 42 }}
-            style={{
-              height: "300px",
-              width: "300px",
-              position: "fixed",
-              left: "50%",
-              transform: "translateX(-50%)",
-            }}
-          >
-            <Profil3D
-              img1={"/profil/profil-ellipse.png"}
-              img2={"/profil/profiln&b-ellipse.png"}
-              ratio={[3.2, 3.2]}
-            />
-          </Canvas>
-        </div>
-      ) : (
-        <Canvas
-          camera={{ position: [0, 0, 4.6], fov: 42 }}
-          style={{
-            height: "92%",
-            width: "40%",
-            position: "fixed",
-            top: "4%",
-          }}
-        >
-          <Profil3D
-            img1={"/profil/profil.jpg"}
-            img2={"/profil/profiln&b.jpg"}
-            ratio={[2.25, 3.5]}
-          />
+  if (animatedElement === "profil") {
+    return (
+      <>
+        {isBelowLG ? (
+          <div className="h-[320px]">
+            <Suspense>
+              <Canvas
+                camera={{ position: [0, 0, 4.6], fov: 42 }}
+                style={{
+                  height: "300px",
+                  width: "300px",
+                  position: "fixed",
+                  left: "50%",
+                  transform: "translateX(-50%)",
+                }}
+              >
+                <Profil3D
+                  img1={"/profil/profil-ellipse.png"}
+                  img2={"/profil/profiln&b-ellipse.png"}
+                  ratio={[3.2, 3.2]}
+                />
+              </Canvas>
+            </Suspense>
+          </div>
+        ) : (
+          <Suspense>
+            <Canvas
+              camera={{ position: [0, 0, 4.6], fov: 42 }}
+              style={{
+                height: "92%",
+                width: "40%",
+                position: "fixed",
+                top: "4%",
+              }}
+            >
+              <Profil3D
+                img1={"/profil/profil.jpg"}
+                img2={"/profil/profiln&b.jpg"}
+                ratio={[2.25, 3.5]}
+              />
+            </Canvas>
+          </Suspense>
+        )}
+      </>
+    );
+  } else if (animatedElement === "title") {
+    return (
+      <>
+        <Canvas camera={{ position: [0, 2, 5.8], fov: 35 }}>
+          <Suspense>
+            <CameraControls />
+            <color attach="background" args={["#171720"]} />
+            <Text
+              font={"fonts/Poppins-Black.ttf"}
+              position-y={1}
+              position-z={2}
+              lineHeight={0.8}
+              letterSpacing={-0.05}
+              textAlign="center"
+            >
+              CREATEUR {"\n"}
+              DE SITE WEB
+              <meshBasicMaterial color={bloomColor} toneMapped={false}>
+                <RenderTexture attach="map">
+                  <color attach="background" args={["#fff"]} />
+                  <Environment preset="sunset" />
+                  <Float floatIntensity={2} rotationIntensity={2}>
+                    <GoodNeighbor
+                      scale={2}
+                      rotation-x={degToRad(25)}
+                      rotation-y={-degToRad(180)}
+                      position-x={0}
+                      position-y={1}
+                      position-z={-1}
+                    />
+                  </Float>
+                </RenderTexture>
+              </meshBasicMaterial>
+            </Text>
+            <mesh position-y={0.23} rotation-x={-Math.PI / 2}>
+              <planeGeometry args={[100, 100]} />
+              <MeshReflectorMaterial
+                blur={[100, 100]}
+                resolution={2048}
+                mixBlur={1}
+                mixStrength={20}
+                roughness={1}
+                depthScale={1}
+                opacity={1}
+                transparent
+                minDepthThreshold={0.4}
+                maxDepthThreshold={1.4}
+                color="#333"
+                metalness={0.5}
+              />
+            </mesh>
+            <Environment preset="sunset" />
+          </Suspense>
+          <EffectComposer>
+            <Bloom mipmapBlur intensity={1.2} />
+          </EffectComposer>
         </Canvas>
-      )}
-    </>
-  );
+      </>
+    );
+  }
 }
--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -33,22 +33,11 @@ export default function Home() {
       className="relative m-auto h-full flex flex-col gap-14 items-center overflow-hidden lg:flex-row"
     >
       <div className="fixed h-[200%] w-[100%] -rotate-[15deg] bg-mainColorContrast -top-[50%] -left-[83%] hidden lg:block"></div>
-      <Experience />
-      <div className="flex flex-col items-center w-full lg:w-2/3 lg:ml-[40%]">
-        <div className="relative flex flex-col items-center justify-center  w-2/3 -top-5 lg:pr-5">
-          <div className="flex justify-center pr-4 lg:block lg:pr-0  ">
-            <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-6xl pl-12">
-              <span className="relative flex text-mainColorContrast before:absolute  before:h-1 before:rounded-full before:bg-mainColorContrast before:-left-12 before:top-[13px] before:w-6 sm:before:w-7 md:before:w-8 lg:before:top-5 lg:before:-left-14 lg:before:w-10 lg:before:h-2">
-                Je m'appelle
-              </span>
-              <span className="flex text-mainColorContrast">
-                {owner.firstname} {owner.lastname}.
-              </span>
-              <span className="flex">Concepteur de</span>
-              <span className="flex">sites web</span>
-            </h1>
-          </div>
-          <p className=" my-5 text-md lg:text-xl lg:w-[80%]">
+      <Experience animatedElement={"profil"} />
+      <div className="flex flex-col items-center w-full lg:w-2/4 lg:ml-[40%]">
+        <div className="relative flex flex-col items-center justify-center h-[600px] w-4/5 -top-5">
+          <Experience animatedElement={"title"} />
+          <p className="my-5 text-md lg:text-xl lg:w-[80%]">
             Je suis concepteur web ainsi que développeur front-end basé à la
             frontière franco-suisse. Je suis passionné par la création
             d'interfaces utilisateur interactives et dynamiques qui captiveront
